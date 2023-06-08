15b8589dcccfd62a038e55b2246abab97bad4a2d
lucasvascrocha <lucas.vasconcelos3@gmail.com>
2023-06-08 18:22:36 -0300

    adjust bkg

--- app/utils/functions.py
+++ app/utils/functions.py
@@ -63,98 +63,98 @@ def build_aggrid_table(df):
 
     return df_filtered
 
-def read_sheets_data():
-    # Configura as credenciais
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('utils/token_gcp.json', scope)
+# def read_sheets_data():
+#     # Configura as credenciais
+#     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+#     credentials = ServiceAccountCredentials.from_json_keyfile_name('utils/token_gcp.json', scope)
 
-    # Autenticação de usuário
-    client = gspread.authorize(credentials)
+#     # Autenticação de usuário
+#     client = gspread.authorize(credentials)
 
-    # Abre a planilha
-    planilha = client.open_by_key('1hWwbZQ6IhsHlYO-EMKc7DyZMxVZmQCs92f6nZGd6q-s')
+#     # Abre a planilha
+#     planilha = client.open_by_key('1hWwbZQ6IhsHlYO-EMKc7DyZMxVZmQCs92f6nZGd6q-s')
 
-    # Seleciona a aba desejada
-    aba = planilha.sheet1
+#     # Seleciona a aba desejada
+#     aba = planilha.sheet1
 
-    # Obtém todos os valores da tabela
-    dados = aba.get_all_values()
+#     # Obtém todos os valores da tabela
+#     dados = aba.get_all_values()
 
-    # Transforma os dados em um dataframe
-    df = pd.DataFrame(dados[1:], columns=dados[0])
+#     # Transforma os dados em um dataframe
+#     df = pd.DataFrame(dados[1:], columns=dados[0])
 
-    return df
+#     return df
 
-def insert_new_Data_in_sheets(df):
-    current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# def insert_new_Data_in_sheets(df):
+#     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Defina o escopo da API e as credenciais do serviço
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('utils/token_gcp.json', scope)
+#     # Defina o escopo da API e as credenciais do serviço
+#     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+#     credentials = ServiceAccountCredentials.from_json_keyfile_name('utils/token_gcp.json', scope)
 
-    # Autorize e abra a planilha
-    client = gspread.authorize(credentials)
+#     # Autorize e abra a planilha
+#     client = gspread.authorize(credentials)
 
-    # Abre a planilha
-    planilha = client.open_by_key('1hWwbZQ6IhsHlYO-EMKc7DyZMxVZmQCs92f6nZGd6q-s')
+#     # Abre a planilha
+#     planilha = client.open_by_key('1hWwbZQ6IhsHlYO-EMKc7DyZMxVZmQCs92f6nZGd6q-s')
 
-    # Seleciona a aba desejada
-    aba = planilha.sheet1
+#     # Seleciona a aba desejada
+#     aba = planilha.sheet1
 
-    # Dados para adicionar
-    novos_dados = [
-        [current_datetime,df['Respostas'][0], df['Respostas'][1], df['Respostas'][2],df['Respostas'][3],df['Respostas'][4], df['Respostas'][5],
-         df['Respostas'][6], df['Respostas'][7], df['Respostas'][8], df['Respostas'][9], df['Respostas'][10], df['Respostas'][11],
-         df['Respostas'][12]
-         ]
+#     # Dados para adicionar
+#     novos_dados = [
+#         [current_datetime,df['Respostas'][0], df['Respostas'][1], df['Respostas'][2],df['Respostas'][3],df['Respostas'][4], df['Respostas'][5],
+#          df['Respostas'][6], df['Respostas'][7], df['Respostas'][8], df['Respostas'][9], df['Respostas'][10], df['Respostas'][11],
+#          df['Respostas'][12]
+#          ]
 
-    ]
+#     ]
 
-    # Encontre a próxima linha vazia
-    proxima_linha = len(aba.get_all_values()) + 1
-    #append
-    aba.update(f'A{proxima_linha}', novos_dados)
+#     # Encontre a próxima linha vazia
+#     proxima_linha = len(aba.get_all_values()) + 1
+#     #append
+#     aba.update(f'A{proxima_linha}', novos_dados)
 
-def insert_only_proposal_data_in_sheets(df_filtered,df_form):
-    # Defina o escopo da API e as credenciais do serviço
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('utils/token_gcp.json', scope)
+# def insert_only_proposal_data_in_sheets(df_filtered,df_form):
+#     # Defina o escopo da API e as credenciais do serviço
+#     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+#     credentials = ServiceAccountCredentials.from_json_keyfile_name('utils/token_gcp.json', scope)
 
-    # Autorize e abra a planilha
-    client = gspread.authorize(credentials)
+#     # Autorize e abra a planilha
+#     client = gspread.authorize(credentials)
 
-    # Abre a planilha
-    planilha = client.open_by_key('1hWwbZQ6IhsHlYO-EMKc7DyZMxVZmQCs92f6nZGd6q-s')
+#     # Abre a planilha
+#     planilha = client.open_by_key('1hWwbZQ6IhsHlYO-EMKc7DyZMxVZmQCs92f6nZGd6q-s')
 
-    # Seleciona a aba desejada
-    aba = planilha.sheet1
+#     # Seleciona a aba desejada
+#     aba = planilha.sheet1
 
-    # Obtém todos os valores da tabela
-    dados = aba.get_all_values()
+#     # Obtém todos os valores da tabela
+#     dados = aba.get_all_values()
 
-    # Transforma os dados em um dataframe
-    df_all = pd.DataFrame(dados[1:], columns=dados[0])
+#     # Transforma os dados em um dataframe
+#     df_all = pd.DataFrame(dados[1:], columns=dados[0])
 
-    #filter aggrid
-    df_loc = df_all.loc[df_all['Descreva o problema ou desafio:'].values == df_filtered['Descreva o problema ou desafio:'].values]
+#     #filter aggrid
+#     df_loc = df_all.loc[df_all['Descreva o problema ou desafio:'].values == df_filtered['Descreva o problema ou desafio:'].values]
 
-    linha = df_loc.index[0] + 2
+#     linha = df_loc.index[0] + 2
 
-    # Obtenha os dados existentes da linha
-    dados_existentes = aba.row_values(linha)
+#     # Obtenha os dados existentes da linha
+#     dados_existentes = aba.row_values(linha)
 
-    # Novos dados a serem adicionados
-    novos_dados = [df_form['Respostas'][0], df_form['Respostas'][1], df_form['Respostas'][2],df_form['Respostas'][3],
-                   df_form['Respostas'][4], df_form['Respostas'][5], df_form['Respostas'][6],df_form['Respostas'][7],
-                   df_form['Respostas'][8], df_form['Respostas'][9], df_form['Respostas'][10],df_form['Respostas'][11],
-                   df_form['Respostas'][12],df_form['Respostas'][13]
-                   ]
+#     # Novos dados a serem adicionados
+#     novos_dados = [df_form['Respostas'][0], df_form['Respostas'][1], df_form['Respostas'][2],df_form['Respostas'][3],
+#                    df_form['Respostas'][4], df_form['Respostas'][5], df_form['Respostas'][6],df_form['Respostas'][7],
+#                    df_form['Respostas'][8], df_form['Respostas'][9], df_form['Respostas'][10],df_form['Respostas'][11],
+#                    df_form['Respostas'][12],df_form['Respostas'][13]
+#                    ]
 
-    # Combine os dados existentes e os novos dados
-    dados_combinados = dados_existentes + novos_dados
+#     # Combine os dados existentes e os novos dados
+#     dados_combinados = dados_existentes + novos_dados
 
-    # Atualize a linha com os dados combinados
-    aba.update(f'A{linha}', [dados_combinados])
+#     # Atualize a linha com os dados combinados
+#     aba.update(f'A{linha}', [dados_combinados])
 
 
 
